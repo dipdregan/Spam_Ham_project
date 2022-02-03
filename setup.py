30820e6162b9417529f882aacce0137ed7ce4fd6
Dipendra Pratap <58151564+dipdregan@users.noreply.github.com>
2022-02-03 06:42:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="docker-heroku",
-    version="0.0.3",
-    description="ML project",
+    version="0.0.1",
+    description="spam_ham project",
     author="Dipendra Pratap", 
     packages=find_packages(),
     license="MIT"
